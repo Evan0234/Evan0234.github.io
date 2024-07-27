eaed0726766cb58957d271a92fc9737b25ea6b21
Evan <106435366+Evan0234@users.noreply.github.com>
2024-07-27 08:47:04 -0400

    Update ban.js

--- ban.js
+++ ban.js
@@ -1,30 +1,17 @@
-const bannedIPs = ['11.22.33.44', '987.654.321.000']; 
-
-function redirectToBannedPage() {
-    if (window.location.pathname !== '/banned.html') {
-        window.location.href = '/banned.html';
-    }
-}
-
+const bannedIPs = ["123.123.123.123", "234.234.234.234"]; // Add IPs you want to ban
 
 fetch('https://api.ipify.org?format=json')
-    .then(response => response.json())
-    .then(data => {
-        const userIP = data.ip;
+  .then(response => response.json())
+  .then(data => {
+    const userIP = data.ip;
+    if (bannedIPs.includes(userIP)) {
+      window.location.href = '/banned';
+    }
+  });
 
-        if (bannedIPs.includes(userIP)) {
-            redirectToBannedPage();
-
-            
-            const observer = new MutationObserver(() => {
-                if (window.location.pathname !== '/banned.html') {
-                    redirectToBannedPage();
-                }
-            });
-
-            observer.observe(document, { subtree: true, childList: true });
-        }
-    })
-    .catch(error => {
-        console.error('Error fetching the IP address:', error);
-    });
+window.addEventListener('hashchange', () => {
+  const userIP = data.ip;
+  if (bannedIPs.includes(userIP)) {
+    window.location.href = '/banned';
+  }
+});
