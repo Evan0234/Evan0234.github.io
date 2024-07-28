b8f97c5133aa693e9148f965afb7011cf6a71e54
Evan <106435366+Evan0234@users.noreply.github.com>
2024-07-27 22:47:44 -0400

    Update ban.js

--- ban.js
+++ ban.js
@@ -1,11 +1,11 @@
 // ban.js
-const bannedIPs = ["123.456.789.0", "987.654.321.0"]; // Add banned IPs here
+const bannedIPs = ["123.456.789.0", "987.654.321.0"]; 
 
-const userIP = fetch("https://api.ipify.org?format=json")
+fetch("https://api.ipify.org?format=json")
     .then(response => response.json())
     .then(data => {
         if (bannedIPs.includes(data.ip)) {
-            window.location.href = "https://zeeps.me/banned";
+            window.location.href = "/banned";
         }
     })
     .catch(error => console.error("Error fetching IP address:", error));
