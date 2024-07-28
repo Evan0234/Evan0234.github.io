e65f57a6382f6bc8939b34f00e2a4920f32e96fd
Evan <106435366+Evan0234@users.noreply.github.com>
2024-07-27 22:12:38 -0400

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDKvMklNFuPJ96u1kZjb2sNsfGBu6_RoK4",
+  authDomain: "zeeps-75fba.firebaseapp.com",
+  projectId: "zeeps-75fba",
+  storageBucket: "zeeps-75fba.appspot.com",
+  messagingSenderId: "593625338479",
+  appId: "1:593625338479:web:3fb257f3d8e10e0aa4f39a",
+  measurementId: "G-NE32QM5B99"
+};
+
+firebase.initializeApp(firebaseConfig);
