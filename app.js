d3b1cbfcb4801ea51804c1a586fa3683a8e5f7c5
Evan <106435366+Evan0234@users.noreply.github.com>
2024-07-27 22:30:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const themeToggle = document.getElementById('themeToggle');
-    const body = document.body;
-
-    themeToggle.addEventListener('click', function() {
-        body.classList.toggle('dark-theme');
-    });
-
     function checkLogin() {
         firebase.auth().onAuthStateChanged(user => {
             if (!user) {
                 window.location.href = 'index.html'; 
-            } else {
-                loadBannedIPs(); 
             }
         });
     }
