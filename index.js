6f1d3fa28909f1b84e275d2c62dd21a6e992076c
Evan <106435366+Evan0234@users.noreply.github.com>
2024-08-13 21:43:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-axios.get('https://phonevalidation.abstractapi.com/v1/?api_key=add_later&phone=14152007986')
+axios.get('https://phonevalidation.abstractapi.com/v1/?api_key=add_later&phone=add_later')
 .then(response => {
 console.log(response.data);
 })
