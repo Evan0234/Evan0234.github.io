6788f66a04241cbf852053f04446d2592b19d131
Evan <106435366+Evan0234@users.noreply.github.com>
2024-09-01 09:32:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,136 +11,129 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // Initialize variables
-const auth = firebase.auth()
-const database = firebase.database()
+const auth = firebase.auth();
+const database = firebase.database();
 
 // Set up our register function
-function register () {
-  // Get all our input fields
-  email = document.getElementById('email').value
-  password = document.getElementById('password').value
-  full_name = document.getElementById('full_name').value
-  favourite_song = document.getElementById('favourite_song').value
-  milk_before_cereal = document.getElementById('milk_before_cereal').value
+function register() {
+    // Get all our input fields
+    email = document.getElementById('email').value;
+    password = document.getElementById('password').value;
+    full_name = document.getElementById('full_name').value;
 
-  // Validate input fields
-  if (validate_email(email) == false || validate_password(password) == false) {
-    alert('Email or Password is Outta Line!!')
-    return
-    // Don't continue running the code
-  }
-  if (validate_field(full_name) == false || validate_field(favourite_song) == false || validate_field(milk_before_cereal) == false) {
-    alert('One or More Extra Fields is Outta Line!!')
-    return
-  }
- 
-  // Move on with Auth
-  auth.createUserWithEmailAndPassword(email, password)
-  .then(function() {
-    // Declare user variable
-    var user = auth.currentUser
-
-    // Add this user to Firebase Database
-    var database_ref = database.ref()
-
-    // Create User data
-    var user_data = {
-      email : email,
-      full_name : full_name,
-      favourite_song : favourite_song,
-      milk_before_cereal : milk_before_cereal,
-      last_login : Date.now()
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+        alert('Email or Password is Outta Line!!');
+        return;
+        // Don't continue running the code
+    }
+    if (validate_field(full_name) == false) {
+        alert('Name field is Outta Line!!');
+        return;
     }
 
-    // Push to Firebase Database
-    database_ref.child('users/' + user.uid).set(user_data)
+    // Move on with Auth
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(function() {
+        // Declare user variable
+        var user = auth.currentUser;
 
-    // DOne
-    alert('User Created!!')
-  })
-  .catch(function(error) {
-    // Firebase will use this to alert of its errors
-    var error_code = error.code
-    var error_message = error.message
+        // Add this user to Firebase Database
+        var database_ref = database.ref();
 
-    alert(error_message)
-  })
+        // Create User data
+        var user_data = {
+            email: email,
+            full_name: full_name,
+            last_login: Date.now()
+        };
+
+        // Push to Firebase Database
+        database_ref.child('users/' + user.uid).set(user_data);
+
+        // Set a cookie to keep the user logged in for a week
+        document.cookie = "loggedIn=true; max-age=" + (7 * 24 * 60 * 60) + "; path=/";
+
+        // Redirect to dashboard
+        window.location.href = 'https://zeeps.me/dashboard';
+    })
+    .catch(function(error) {
+        // Firebase will use this to alert of its errors
+        var error_code = error.code;
+        var error_message = error.message;
+
+        alert(error_message);
+    });
 }
 
 // Set up our login function
-function login () {
-  // Get all our input fields
-  email = document.getElementById('email').value
-  password = document.getElementById('password').value
+function login() {
+    // Get all our input fields
+    email = document.getElementById('email').value;
+    password = document.getElementById('password').value;
 
-  // Validate input fields
-  if (validate_email(email) == false || validate_password(password) == false) {
-    alert('Email or Password is Outta Line!!')
-    return
-    // Don't continue running the code
-  }
-
-  auth.signInWithEmailAndPassword(email, password)
-  .then(function() {
-    // Declare user variable
-    var user = auth.currentUser
-
-    // Add this user to Firebase Database
-    var database_ref = database.ref()
-
-    // Create User data
-    var user_data = {
-      last_login : Date.now()
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+        alert('Email or Password is Outta Line!!');
+        return;
+        // Don't continue running the code
     }
 
-    // Push to Firebase Database
-    database_ref.child('users/' + user.uid).update(user_data)
+    auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+        // Declare user variable
+        var user = auth.currentUser;
 
-    // DOne
-    alert('User Logged In!!')
+        // Add this user to Firebase Database
+        var database_ref = database.ref();
 
-  })
-  .catch(function(error) {
-    // Firebase will use this to alert of its errors
-    var error_code = error.code
-    var error_message = error.message
+        // Create User data
+        var user_data = {
+            last_login: Date.now()
+        };
 
-    alert(error_message)
-  })
+        // Push to Firebase Database
+        database_ref.child('users/' + user.uid).update(user_data);
+
+        // Set a cookie to keep the user logged in for a week
+        document.cookie = "loggedIn=true; max-age=" + (7 * 24 * 60 * 60) + "; path=/";
+
+        // Redirect to dashboard
+        window.location.href = 'https://zeeps.me/dashboard';
+    })
+    .catch(function(error) {
+        // Firebase will use this to alert of its errors
+        var error_code = error.code;
+        var error_message = error.message;
+
+        alert(error_message);
+    });
 }
 
-
-
+// Redirect if logged in or not
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        if (window.location.pathname === '/login' || window.location.pathname === '/') {
+            window.location.href = 'https://zeeps.me/dashboard';
+        }
+    } else {
+        if (window.location.pathname === '/dashboard') {
+            window.location.href = 'https://zeeps.me/login';
+        }
+    }
+});
 
 // Validate Functions
 function validate_email(email) {
-  expression = /^[^@]+@\w+(\.\w+)+\w$/
-  if (expression.test(email) == true) {
-    // Email is good
-    return true
-  } else {
-    // Email is not good
-    return false
-  }
+    expression = /^[^@]+@\w+(\.\w+)+\w$/;
+    return expression.test(email);
 }
 
 function validate_password(password) {
-  // Firebase only accepts lengths greater than 6
-  if (password < 6) {
-    return false
-  } else {
-    return true
-  }
+    // Firebase only accepts lengths greater than 6
+    return password.length >= 6;
 }
 
 function validate_field(field) {
-  if (field == null) {
-    return false
-  }
-
-  if (field.length <= 0) {
-    return false
-  } else {
-    return true
-  }
+    return field != null && field.length > 0;
 }
