8ef446d54a4925d354c79ae6fa9e2829ec05086f
Evan <106435366+Evan0234@users.noreply.github.com>
2024-09-02 10:00:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var firebaseConfig = {
     apiKey: "AIzaSyAjl5C7TvjmtxPc4_eno6vRMIVjciLiV04",
     authDomain: "zeeplogin.firebaseapp.com",
     projectId: "zeeplogin",
-    storageBucket: "zeeplogin.appspot.com",
+    storageBucket: "zeeplogin",
     messagingSenderId: "343221159933",
     appId: "1:343221159933:web:e6c3e1e7ec6161a48dfb94",
     measurementId: "G-DE7X1YKVGY"
@@ -15,132 +15,128 @@ const auth = firebase.auth()
 const database = firebase.database()
 
 // Set up our register function
-function register () {
-  // Get all our input fields
-  email = document.getElementById('email').value
-  password = document.getElementById('password').value
-  full_name = document.getElementById('full_name').value
-  favourite_song = document.getElementById('favourite_song').value
-  milk_before_cereal = document.getElementById('milk_before_cereal').value
+function register() {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+    full_name = document.getElementById('full_name').value
+    favourite_song = document.getElementById('favourite_song').value
+    milk_before_cereal = document.getElementById('milk_before_cereal').value
 
-  // Validate input fields
-  if (validate_email(email) == false || validate_password(password) == false) {
-    alert('Email or Password is Outta Line!!')
-    return
-    // Don't continue running the code
-  }
-  if (validate_field(full_name) == false || validate_field(favourite_song) == false || validate_field(milk_before_cereal) == false) {
-    alert('One or More Extra Fields is Outta Line!!')
-    return
-  }
- 
-  // Move on with Auth
-  auth.createUserWithEmailAndPassword(email, password)
-  .then(function() {
-    // Declare user variable
-    var user = auth.currentUser
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+        alert('Email or Password is Outta Line!!')
+        return
+    }
+    if (validate_field(full_name) == false || validate_field(favourite_song) == false || validate_field(milk_before_cereal) == false) {
+        alert('One or More Extra Fields is Outta Line!!')
+        return
+    }
 
-    // Add this user to Firebase Database
-    var database_ref = database.ref()
+    // Create the user with email and password
+    auth.createUserWithEmailAndPassword(email, password)
+        .then(function() {
+            var user = auth.currentUser;
+
+            // Send verification email
+            user.sendEmailVerification().then(function() {
+                alert('Verification Email Sent! Please check your inbox.');
+
+                // Set a listener to detect changes in the user's authentication state
+                auth.onAuthStateChanged(function(user) {
+                    if (user) {
+                        if (user.emailVerified) {
+                            // If email is verified, add user to the database
+                            saveUserToDatabase(user, full_name, favourite_song, milk_before_cereal);
+                        } else {
+                            alert('Please verify your email before proceeding.');
+                        }
+                    }
+                });
+            }).catch(function(error) {
+                console.error('Error while sending verification email:', error);
+                alert(error.message);
+            });
+        })
+        .catch(function(error) {
+            // Firebase will use this to alert of its errors
+            var error_code = error.code;
+            var error_message = error.message;
+
+            alert(error_message);
+        });
+}
+
+// Function to save user data to Firebase Database
+function saveUserToDatabase(user, full_name, favourite_song, milk_before_cereal) {
+    var database_ref = database.ref();
 
     // Create User data
     var user_data = {
-      email : email,
-      full_name : full_name,
-      favourite_song : favourite_song,
-      milk_before_cereal : milk_before_cereal,
-      last_login : Date.now()
-    }
+        email: user.email,
+        full_name: full_name,
+        favourite_song: favourite_song,
+        milk_before_cereal: milk_before_cereal,
+        last_login: Date.now()
+    };
 
     // Push to Firebase Database
-    database_ref.child('users/' + user.uid).set(user_data)
+    database_ref.child('users/' + user.uid).set(user_data);
 
-    // DOne
-    alert('User Created!!')
-  })
-  .catch(function(error) {
-    // Firebase will use this to alert of its errors
-    var error_code = error.code
-    var error_message = error.message
-
-    alert(error_message)
-  })
+    alert('User Created and Email Verified!!');
 }
 
 // Set up our login function
-function login () {
-  // Get all our input fields
-  email = document.getElementById('email').value
-  password = document.getElementById('password').value
+function login() {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
 
-  // Validate input fields
-  if (validate_email(email) == false || validate_password(password) == false) {
-    alert('Email or Password is Outta Line!!')
-    return
-    // Don't continue running the code
-  }
-
-  auth.signInWithEmailAndPassword(email, password)
-  .then(function() {
-    // Declare user variable
-    var user = auth.currentUser
-
-    // Add this user to Firebase Database
-    var database_ref = database.ref()
-
-    // Create User data
-    var user_data = {
-      last_login : Date.now()
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+        alert('Email or Password is Outta Line!!')
+        return
     }
 
-    // Push to Firebase Database
-    database_ref.child('users/' + user.uid).update(user_data)
+    auth.signInWithEmailAndPassword(email, password)
+        .then(function() {
+            var user = auth.currentUser;
 
-    // DOne
-    alert('User Logged In!!')
+            if (user.emailVerified) {
+                // User is verified, proceed with login
+                var database_ref = database.ref();
 
-  })
-  .catch(function(error) {
-    // Firebase will use this to alert of its errors
-    var error_code = error.code
-    var error_message = error.message
+                // Create User data
+                var user_data = {
+                    last_login: Date.now()
+                };
 
-    alert(error_message)
-  })
+                // Push to Firebase Database
+                database_ref.child('users/' + user.uid).update(user_data);
+
+                alert('User Logged In!!');
+            } else {
+                alert('Please verify your email before logging in.');
+            }
+        })
+        .catch(function(error) {
+            var error_code = error.code;
+            var error_message = error.message;
+
+            alert(error_message);
+        });
 }
-
-
-
 
 // Validate Functions
 function validate_email(email) {
-  expression = /^[^@]+@\w+(\.\w+)+\w$/
-  if (expression.test(email) == true) {
-    // Email is good
-    return true
-  } else {
-    // Email is not good
-    return false
-  }
+    expression = /^[^@]+@\w+(\.\w+)+\w$/;
+    return expression.test(email);
 }
 
 function validate_password(password) {
-  // Firebase only accepts lengths greater than 6
-  if (password < 6) {
-    return false
-  } else {
-    return true
-  }
+    return password.length >= 6;
 }
 
 function validate_field(field) {
-  if (field == null) {
-    return false
-  }
-
-  if (field.length <= 0) {
-    return false
-  } else {
-    return true
-  }
+    return field != null && field.length > 0;
 }
