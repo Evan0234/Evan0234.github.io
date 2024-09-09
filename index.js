3318e408f079b2d3d8de680351486192d24a2b2c
Evan <106435366+Evan0234@users.noreply.github.com>
2024-09-09 18:55:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,6 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-// Show warning message on page load
-window.onload = function() {
-    alert("⚠️ WARNING ⚠️\n\nNEVER SHARE ANYTHING FROM THE TERMINAL. IF SOMEBODY ASKED YOU TO GET SOMETHING HERE, IT'S A SCAM!");
-}
-
 // Register function
 function register() {
     try {
